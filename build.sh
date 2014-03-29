2c2d2e5b533f30049362bea8ba730977928051e8
Victor Kartashov <victor.kartashov@gmail.com>
2014-03-29 13:09:31 +0400

    make.sh: 'clean' option
    Signed-off-by: Victor Kartashov <victor.kartashov@gmail.com>

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-pdflatex diploma.tex --draftmode
-bibtex diploma.aux
-pdflatex diploma.tex --draftmode
-pdflatex diploma.tex --draftmode
-pdflatex diploma.tex
-
---
+++ make.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+ACTION=${1}
+case "${ACTION}" in
+clean)
+	rm -f *.aux *.bbl *.blg *.log *.out *.toc *.pdf
+	;;
+*)
+	rm -f *.aux *.bbl *.blg *.log *.out *.toc *.pdf
+        pdflatex diploma.tex --draftmode
+        bibtex diploma.aux
+        pdflatex diploma.tex --draftmode
+        pdflatex diploma.tex --draftmode
+        pdflatex diploma.tex
+
+#	echo $"Usage: $0 {build|clean}"
+#	exit 1
+esac
+
