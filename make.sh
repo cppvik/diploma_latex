dd829e5f736097a7c79b156aa18f5fb8e0bdfb3a
Victor Kartashov <victor.kartashov@gmail.com>
2014-04-01 20:11:55 +0400

    economics: расчёт заработной платы make.sh: ускорение компиляции в ущерб списку литературы, полная компиляция по make.sh build tables: более лучшее центрирование ячеек reductions: сортировка списка по алфавиту (в соответствии с требованиями)
    Signed-off-by: Victor Kartashov <victor.kartashov@gmail.com>

--- make.sh
+++ make.sh
@@ -2,6 +2,14 @@
 
 ACTION=${1}
 case "${ACTION}" in
+build)
+	rm -f *.aux *.bbl *.blg *.log *.out *.toc *.pdf
+        pdflatex diploma.tex --draftmode
+        bibtex diploma.aux
+        pdflatex diploma.tex --draftmode
+        pdflatex diploma.tex --draftmode
+        pdflatex diploma.tex
+	;;
 clean)
 	rm -f *.aux *.bbl *.blg *.log *.out *.toc *.pdf
 	;;
@@ -9,8 +17,6 @@ clean)
 	rm -f *.aux *.bbl *.blg *.log *.out *.toc *.pdf
         pdflatex diploma.tex --draftmode
         bibtex diploma.aux
-        pdflatex diploma.tex --draftmode
-        pdflatex diploma.tex --draftmode
         pdflatex diploma.tex
 
 #	echo $"Usage: $0 {build|clean}"
