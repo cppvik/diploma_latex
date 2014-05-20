6c5cda3d6f450333e39819c1129c67d86c93c921
Victor Kartashov <victor.kartashov@gmail.com>
2014-05-20 19:30:20 +0400

    research:	добавлены картинки из слайдов: цепочка хэшей и meld; исправлены мелкие недочёты diploma:	список сокращений вынесен перед содержанием make.sh:	clean больше не удаляет pdf (это делает только build)
    Signed-off-by: Victor Kartashov <victor.kartashov@gmail.com>

--- make.sh
+++ make.sh
@@ -11,7 +11,7 @@ build)
         pdflatex diploma.tex
 	;;
 clean)
-	rm -f *.aux *.bbl *.blg *.log *.out *.toc *.pdf *.lot *.lof
+	rm -f *.aux *.bbl *.blg *.log *.out *.toc *.lot *.lof
 	;;
 *)
 #	rm -f *.aux *.bbl *.blg *.log *.out *.toc *.pdf *.lot *.lof
