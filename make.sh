7d858b3a71581fe8e8b15bd71cbb18032bc2b980
Victor Kartashov <victor.kartashov@gmail.com>
2014-06-08 13:53:39 +0400

    appendix:	добавлено приложение с исходными текстами technologic:	добавлена ссылка на приложение abstract:	заголовок по центру, мелкие исправления make.sh:	исправление для корректной работы хайлайтера исходных текстов minted
    Signed-off-by: Victor Kartashov <victor.kartashov@gmail.com>

--- make.sh
+++ make.sh
@@ -4,20 +4,20 @@ ACTION=${1}
 case "${ACTION}" in
 build)
 	rm -f *.aux *.bbl *.blg *.log *.out *.toc *.pdf *.lot *.lof
-        pdflatex diploma.tex --draftmode
+        pdflatex -shell-escape diploma.tex --draftmode
         bibtex diploma.aux
-        pdflatex diploma.tex --draftmode
-        pdflatex diploma.tex --draftmode
-        pdflatex diploma.tex
+        pdflatex -shell-escape diploma.tex --draftmode
+        pdflatex -shell-escape diploma.tex --draftmode
+        pdflatex -shell-escape diploma.tex
 	;;
 clean)
 	rm -f *.aux *.bbl *.blg *.log *.out *.toc *.lot *.lof
 	;;
 *)
 #	rm -f *.aux *.bbl *.blg *.log *.out *.toc *.pdf *.lot *.lof
-        pdflatex diploma.tex --draftmode
+        pdflatex -shell-escape diploma.tex --draftmode
         bibtex diploma.aux
-        pdflatex diploma.tex
+        pdflatex -shell-escape diploma.tex
 
 #	echo $"Usage: $0 {build|clean}"
 #	exit 1
